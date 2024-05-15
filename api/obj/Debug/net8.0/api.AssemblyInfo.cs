d4154160023276a2cd5bb32a64e173a0c3d389bf
MuradMT <mammadzade03@gmail.com>
2024-05-15 17:11:05 +0400

    Learned one to many relationship,used data annotations in some model properties in order to obtain specific sql data type.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0c29c79dde71ac3ab63a8d30a28134c4839c9d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5188a9cdd30ce520e03621b52cd9424992e12cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
