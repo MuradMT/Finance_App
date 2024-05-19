b92d3e02f413154309c5af5a75772c7e9c98b4f8
MuradMT <mammadzade03@gmail.com>
2024-05-19 16:59:26 +0400

    Created Income statement.Learned about 429 hhtp code(too many requests).

--- frontend/src/API/api.tsx
+++ frontend/src/API/api.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from '../Types/company'
+import { CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from '../Types/company'
 import { error } from 'console';
 interface SearchResponse {
     data: CompanySearch[]
@@ -36,6 +36,17 @@ export const getKeyMetrics=async (query:string)=>{
     try {
         const data = await axios.get<CompanyKeyMetrics[]>(
             `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        return data;
+    } catch (error:any) {
+        console.log("error message from API: ", error);
+    }
+}
+
+export const getIncomeStatement=async (query:string)=>{
+    try {
+        const data = await axios.get<CompanyIncomeStatement[]>(
+            `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
         );
         return data;
     } catch (error:any) {
--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -1,44 +1,30 @@
-import React from "react";
-import { testIncomeStatementData } from "./testData";
 
-interface Props {}
+interface Props {
+  data: any;
+  config: any;
+}
 
-type Company = (typeof data)[0];
 
-const data = testIncomeStatementData;
 
-const configs = [
-  {
-    Label: "Year",
-    render: (company: Company) => company.acceptedDate,
-  },
-  {
-    Label: "Cost of Revenue",
-    render: (company: Company) => company.revenue,
-  },
-];
-
-const Table = (props: Props) => {
-  const renderedRows = data.map((company: Company) => {
+const Table = ({config,data}: Props) => {
+  const renderedRows = data.map((company: any) => {
     return (
       <tr key={company.cik}>
-        {configs.map((config: any) => {
-          return (
-            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-              {config.render(company)}
-            </td>
-          );
-        })}
+        {
+            config.map((config: any) =>{
+                return <td className="p-3">{config.render(company)}</td>
+            })
+        }
       </tr>
     );
   });
-  const renderedHeaders = configs.map((config: any) => {
+  const renderedHeaders = config.map((config: any) => {
     return (
       <th
         className="p-4 uppercase tracking-wider text-xs text-left font-medium text-gray-500"
-        key={config.Label}
+        key={config.label}
       >
-        {config.Label}
+        {config.label}
       </th>
     );
   });
